ab0917176ea0bc4792a07d24d6a6bced942ffd49
Jason Bacon <bacon4000@github.com>
2018-10-25 00:39:05 +0000

    Bump version following commit
    git-svn-id: file:///home/bacon/SVN/desktop-installer/desktop-installer-0.6.3@164 f1aa7019-f849-e211-8a70-0017315c2eee

--- Scripts/battery_shutdown.sh
+++ Scripts/battery_shutdown.sh
@@ -21,24 +21,26 @@ battery_check_interval=60
 
 set -x
 while true; do
-    battery_time=$( /sbin/sysctl -n hw.acpi.battery.time )
     ac_power=$( /sbin/sysctl -n hw.acpi.acline )
     
-    if [ $ac_power = 0 ] && [ $battery_time -ge 0 ]; then
-	if [ $battery_time -le $critical_time ]; then
-	    printf "Battery level is critical.\nInsert power plug within $shutdown_delay seconds to avoid shutdown.\n" | wall
+    if [ $ac_power = 0 ]; then
+        battery_time=$( /sbin/sysctl -n hw.acpi.battery.time )
+        if [ $battery_time -ge 0 ]; then
+	    if [ $battery_time -le $critical_time ]; then
+	        printf "Battery level is critical.\nInsert power plug within $shutdown_delay seconds to avoid shutdown.\n" | wall
 	    
-	    # Give user time to plug in
-	    /bin/sleep $shutdown_delay
+	        # Give user time to plug in
+	        /bin/sleep $shutdown_delay
 	    
-	    # Recheck for external power before shutting down
-	    ac_power=$( /sbin/sysctl -n hw.acpi.acline )
-	    if [ $ac_power = 0 ]
-		then /sbin/shutdown -p +1
+	        # Recheck for external power before shutting down
+	        ac_power=$( /sbin/sysctl -n hw.acpi.acline )
+	        if [ $ac_power = 0 ]
+		    then /sbin/shutdown -p +1
+	        fi
 	    fi
-	fi
-    elif [ $battery_time -le $warning_time ]; then
-	printf "Warning: Battery is at $battery_time minutes.\n" | wall
+        elif [ $battery_time -le $warning_time ]; then
+	    printf "Warning: Battery is at $battery_time minutes.\n" | wall
+        fi
     fi
     /bin/sleep $battery_check_interval
 done
