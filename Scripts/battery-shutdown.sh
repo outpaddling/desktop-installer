bd13aed6694970ea04d92e04f5f7ec6cebaf00ae
Jason Bacon <bacon@coral.acadix.biz>
2021-04-22 12:44:33 -0500

    battery-shutdown.sh: Disable -x, rename with - in place of _

--- Scripts/battery-shutdown.sh
+++ Scripts/battery-shutdown.sh
@@ -19,28 +19,27 @@ shutdown_delay=60
 # seconds to pause between script runs
 battery_check_interval=60
 
-set -x
 while true; do
     ac_power=$( /sbin/sysctl -n hw.acpi.acline )
     
     if [ $ac_power = 0 ]; then
-        battery_time=$( /sbin/sysctl -n hw.acpi.battery.time )
-        if [ $battery_time -ge 0 ]; then
+	battery_time=$( /sbin/sysctl -n hw.acpi.battery.time )
+	if [ $battery_time -ge 0 ]; then
 	    if [ $battery_time -le $critical_time ]; then
-	        printf "Battery level is critical.\nInsert power plug within $shutdown_delay seconds to avoid shutdown.\n" | wall
+		printf "Battery level is critical.\nInsert power plug within $shutdown_delay seconds to avoid shutdown.\n" | wall
 	    
-	        # Give user time to plug in
-	        /bin/sleep $shutdown_delay
+		# Give user time to plug in
+		/bin/sleep $shutdown_delay
 	    
-	        # Recheck for external power before shutting down
-	        ac_power=$( /sbin/sysctl -n hw.acpi.acline )
-	        if [ $ac_power = 0 ]
+		# Recheck for external power before shutting down
+		ac_power=$( /sbin/sysctl -n hw.acpi.acline )
+		if [ $ac_power = 0 ]
 		    then /sbin/shutdown -p +1
-	        fi
+		fi
 	    fi
-        elif [ $battery_time -le $warning_time ]; then
+	elif [ $battery_time -le $warning_time ]; then
 	    printf "Warning: Battery is at $battery_time minutes.\n" | wall
-        fi
+	fi
     fi
     /bin/sleep $battery_check_interval
 done
