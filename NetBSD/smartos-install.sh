23a6ee4d70cea995160f8b049b5f2453ddc4ee1f
Jason W. Bacon <bacon4000@gmail.com>
2023-07-10 11:50:42 -0500

    smartos-install.sh: Script from smartos.org to install on NetBSD

---
+++ NetBSD/smartos-install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+#
+# Copy and paste the lines below to install the NetBSD/amd64 set.
+#
+BOOTSTRAP_TAR="bootstrap-netbsd-trunk-x86_64-20230710.tar.gz"
+BOOTSTRAP_SHA="86aed1031713e15e7f411d3622124673797cce0c"
+
+# Download the bootstrap kit to the current directory.
+ftp https://pkgsrc.smartos.org/packages/NetBSD/bootstrap/${BOOTSTRAP_TAR}
+
+# Verify the SHA1 checksum.
+echo "${BOOTSTRAP_SHA} ${BOOTSTRAP_TAR}" | sha1 -c
+
+# Verify PGP signature.  This step is optional, and requires gpg.
+#ftp https://pkgsrc.smartos.org/packages/NetBSD/bootstrap/${BOOTSTRAP_TAR}.asc
+#ftp -Vo - https://pkgsrc.smartos.org/pgp/C72658C9.asc | gpg2 --import
+#gpg2 --verify ${BOOTSTRAP_TAR}.asc ${BOOTSTRAP_TAR}
+
+#
+# Remove any existing packages.  Note also that the bootstrap kit will
+# install its own copies of the security/mozilla-rootcerts certificates
+# into the /etc/openssl/certs/ directory.
+#
+rm -rf /usr/pkg /var/db/pkg /var/db/pkgin
+
+# Install bootstrap kit to /usr/pkg
+tar -zxpf ${BOOTSTRAP_TAR} -C /
