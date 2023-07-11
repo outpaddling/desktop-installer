320321c607fb33ad132422beb0e5cca0e8aabdb3
Jason W. Bacon <bacon4000@gmail.com>
2023-07-11 13:58:42 -0500

    FreeBSD: Add zenity notifications to battery-shutdown.sh

--- Scripts/battery-shutdown.sh
+++ Scripts/battery-shutdown.sh
@@ -7,17 +7,23 @@
 #   History
 #   Date        Who         Change
 #   Dec 2010    J Bacon     Derived from script by DutchDaemon
+#   Jul 2023    J Bacon     Add zenity messages
 ##########################################################################
 
-# Battery level critical %
+# Important remaining run times
 critical_time=5
-warning_time=15
+warning_time=20
+low_time=30
+low_notified=no
 
 # Time between warning user and initiating shutdown
-shutdown_delay=60
+shutdown_delay=30
 
 # seconds to pause between script runs
 battery_check_interval=60
+
+log="/var/log/battery-shutdown"
+printf "Started $(date)\n" > $log
 
 while true; do
     ac_power=$( /sbin/sysctl -n hw.acpi.acline )
@@ -26,19 +32,31 @@ while true; do
 	battery_time=$( /sbin/sysctl -n hw.acpi.battery.time )
 	if [ $battery_time -ge 0 ]; then
 	    if [ $battery_time -le $critical_time ]; then
-		printf "Battery level is critical.\nInsert power plug within $shutdown_delay seconds to avoid shutdown.\n" | wall
-	    
-		# Give user time to plug in
-		/bin/sleep $shutdown_delay
+		printf "Battery critical $(date)\n" >> $log
 	    
 		# Recheck for external power before shutting down
 		ac_power=$( /sbin/sysctl -n hw.acpi.acline )
 		if [ $ac_power = 0 ]
-		    then /sbin/shutdown -p +1
+		    then /sbin/shutdown -p now
 		fi
+	    elif [ $battery_time -le $warning_time ]; then
+		printf "Battery warning $(date)\n" >> $log
+		display_users=$(ps -aexwwj | grep "DISPLAY=$DISPLAY_ID" | awk '{ print $1 }' | sort -u)
+		for user in $display_users; do
+		    if su -l $user -c "zenity --display=:0 --warning --text='The battery is very low.\nThe computer will be shut down\nsoon to prevent battery damage.'" > /dev/null; then
+			break
+		    fi
+		done
+	    elif [ $battery_time -le $low_time ] && [ $low_notified = no ]; then
+		printf "Battery low $(date)\n" >> $log
+		display_users=$(ps -aexwwj | grep "DISPLAY=$DISPLAY_ID" | awk '{ print $1 }' | sort -u)
+		for user in $display_users; do
+		    if su -l $user -c "zenity --display=:0 --warning --text='The battery is getting low.\nConsider plugging in.'" > /dev/null; then
+			low_notified=yes
+			break
+		    fi
+		done
 	    fi
-	elif [ $battery_time -le $warning_time ]; then
-	    printf "Warning: Battery is at $battery_time minutes.\n" | wall
 	fi
     fi
     /bin/sleep $battery_check_interval
